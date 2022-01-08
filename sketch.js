afe2c67631438f4f4a0a60f3960de0a260087a0a
Kavya1701 <97327131+Kavya1701@users.noreply.github.com>
2022-01-08 10:02:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var box;
+var box2;
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,30,30);
+  box2 = createSprite(100,100,20,20);
+}
+
+function draw() 
+{
+   background(30);
+
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+    box.position.x = box.position.x +5;
+  }
+
+    if (keyIsDown(LEFT_ARROW)) 
+  {
+    box.position.x= box.position.x -5;
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    box.position.y = box.position.y - 5;
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    box.position.y = box.position.y + 5;
+  }
+
+  drawSprites();
+}
+
+
+
+
